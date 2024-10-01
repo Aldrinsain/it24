c98cb693a17cd31138d01b52236f7a084daf1261
aldrinsain <angelasain86@gmail.com>
2024-10-01 11:16:00 +0800

    Added Arithmetic and Logical Operators

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ let greeting = "Hello, World!"; // String
 let numbersArray = [1, 2, 3];   // Array
 let user = { name: "Alice", age: 30 }; // Object
 
+// b. Arithmetic and Logical Operators
+let sum = number + 10;                     // Addition
+let isPositive = number > 0;                // Logical Operator
+let product = number * 2;                   // Multiplication
