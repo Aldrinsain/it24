7e26d1a97cae12defaf18607330af33ec5aa886f
aldrinsain <angelasain86@gmail.com>
2024-10-01 11:15:00 +0800

    Added data types

---
+++ app.js
@@ -0,0 +1,7 @@
+// a. Data Types
+let number = 5;                  // Number
+let isEven = false;             // Boolean
+let greeting = "Hello, World!"; // String
+let numbersArray = [1, 2, 3];   // Array
+let user = { name: "Alice", age: 30 }; // Object
+
